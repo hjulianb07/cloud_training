d4036546373cb054adc7a6312d8eb78fb6effa44
Adriana <apatricirojas@ucundinamarca.edu.co>
2023-09-25 10:42:34 -0500

    primer commit

---
+++ project1_Adri.py
@@ -0,0 +1,12 @@
+def division(num1,num2):
+    return num1/num2
+print('Escriba numero: ')
+num1 = int(input()) 
+num2 = int(input())
+
+print ('Division 4')
+
+operacion = input()
+
+if operacion =='4':
+    print(num1,'/',num2,'=',division(num1,num2))
